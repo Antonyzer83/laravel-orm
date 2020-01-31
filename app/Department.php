c87cdf3c99431c8f2bbff0fbecbc49841b79416f
Antonyzer83 <antony@castaner-vm.local>
2020-01-31 18:04:02 +0100

    Add Department model

---
+++ app/Department.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['dept_name'];
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'dept_no';
+
+    /**
+     * The relationship between departments and employees for managers
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function dept_managers()
+    {
+        return $this->belongsToMany('App\Employee', 'dept_manager', 'dept_no', 'emp_no');
+    }
+
+    /**
+     * The relationship between departments and employees for employees
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function dept_emps()
+    {
+        return $this->belongsToMany('App\Employee', 'dept_emp', 'dept_no', 'emp_no');
+    }
+}
