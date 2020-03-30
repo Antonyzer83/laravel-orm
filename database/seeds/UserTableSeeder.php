3f464b30107e9214b56e87a56adfacf047078a45
Antony CASTANER <antony.castanerpynov.com>
2020-03-30 16:15:50 +0200

    Seed all current managers

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -14,9 +14,21 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         $employees = Employee::orderBy('emp_no')
-            ->limit(100)
+            ->limit(50)
             ->get();
 
+        $managers = Employee::join('titles', 'titles.emp_no', 'employees.emp_no')
+            ->where([
+                ['title', 'Manager'],
+                ['to_date', '9999-01-01'],
+            ])
+            ->get();
+
+        $this->createUsers($employees);
+        $this->createUsers($managers);
+    }
+
+    private function createUsers($employees) {
         foreach ($employees as $employee) {
             $data = [];
             $data['emp_no'] = $employee->emp_no;
